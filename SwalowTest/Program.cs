a88f3a0ac5405958035545b861957e8cad636ef4
katryn36 <katryn36@gmail.com>
2019-10-13 22:25:04 +0300

    tests

--- SwalowTest/Program.cs
+++ SwalowTest/Program.cs
@@ -1,19 +1,22 @@
 ﻿using Swalow;
 using System;
 
+/// <summary>
+/// test assemdly
+/// </summary>
 namespace SwalowTest
 {
     /// <summary>
-    /// 
+    /// test class
     /// </summary>
     public class Program
     {
         /// <summary>
-        /// 
+        /// test varible
         /// </summary>
         public static int ver;
         /// <summary>
-        /// 
+        /// test method
         /// </summary>
         /// <param name="args">ipp</param>
         static void Main(string[] args)
@@ -28,6 +31,14 @@ namespace SwalowTest
             }
             
             Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// test 2 method
+        /// </summary>
+        /// <returns>test returns</returns>
+        static string TestMethod(){
+            return "";
         }
     }
 }
